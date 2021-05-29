63cb7ec0f1a832125d18bd544604a5482d017b64
NikittaThomas342 <nikita_tho@cmu.ac.th>
2021-05-29 22:14:48 +0700

    Final ver

---
+++ script.js
@@ -0,0 +1,47 @@
+document.getElementById('item1').addEventListener('click',function(e){
+        alert('Rose')
+    })
+
+    document.getElementById('item2').addEventListener('click',function(e){
+        alert('Rose')
+    })
+
+    document.getElementById('item3').addEventListener('click',function(e){
+        alert('Rose')
+    })
+
+    document.getElementById('item4').addEventListener('click',function(e){
+        alert('Rose')
+    })
+
+    document.getElementById('item5').addEventListener('click',function(e){
+        alert('Rose')
+    })
+
+    document.getElementById('item6').addEventListener('click',function(e){
+        alert('Rose')
+    })
+
+    document.getElementById('item7').addEventListener('click',function(e){
+        alert('Rose')
+    })
+
+    document.getElementById('item8').addEventListener('click',function(e){
+        alert('Rose')
+    })
+
+    document.getElementById('item9').addEventListener('click',function(e){
+        alert('Rose')
+    })
+
+    document.getElementById('item10').addEventListener('click',function(e){
+        alert('Rose')
+    })
+
+    document.getElementById('item11').addEventListener('click',function(e){
+        alert('Rose')
+    })
+
+    document.getElementById('item12').addEventListener('click',function(e){
+        alert('Rose')
+    })
